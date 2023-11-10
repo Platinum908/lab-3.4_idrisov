81e37d229f8ffa60b98ad9951d2f17cf9ab32e84
Administrator <Administrator@WIN-QH3366O2A51>
2023-11-10 23:59:12 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿//высокий уровень, 3.4, 4 вар
+
+Console.WriteLine($"|    x   |     f(x)    |");
+for( double x = -2; x <= 3; x+=0.5)
+{
+    double n = 0;
+    for(double k = 1;k <= 5; k++) 
+    {
+        double sumF = 1;
+        for(double factorial = 1; factorial <= k+1; factorial++) sumF *= factorial;
+        n += (Math.Pow(-2, k + 1) * sumF / Math.Pow(x + 2.5, k + 1));
+    }
+    Console.WriteLine($"|{x,6:F2} {"|",2}{n,11:F2}{"  |",2}");
+}
